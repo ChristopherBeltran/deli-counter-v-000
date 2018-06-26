5c6ba6d99dd94c35302da6c3c086255763a9344c
Christopher Beltran <christod.beltran@gmail.com>
2018-06-26 06:31:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   this_line = []
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else 
     katz_deli.each_with_index do |person, number|
       this_line << "#{number + 1}. #{person}"
