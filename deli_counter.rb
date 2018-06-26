33f442c52e85029514990d69ebd87f046ac7a3ba
Christopher Beltran <christod.beltran@gmail.com>
2018-06-26 06:30:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,12 @@
-# Write your code here.
-def oxford_comma(array)
-  case
-  when array.length > 2
-  array[0..-2].join(", ") + ", and " + array.last
-  
-  when array.length == 2
-    array.join(" and ")
-  when array.length == 1 
-    array.join
+def line(katz_deli)
+  this_line = []
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else 
+    katz_deli.each_with_index do |person, number|
+      this_line << "#{number + 1}. #{person}"
+      the_line = this_line.join
+      return "The line is currently: #{the_line}" 
+    end 
+  end 
 end 
-end
-katzDeli = []
-
-def line(katzDeli)
-  case
-  when katzDeli.length >
-end
-
