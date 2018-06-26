1ea23ace7b92d9184eb4454d72220ca2c5be3fe5
Christopher Beltran <christod.beltran@gmail.com>
2018-06-26 06:00:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def oxford_comma(array)
+  case
+  when array.length > 2
+  array[0..-2].join(", ") + ", and " + array.last
+  
+  when array.length == 2
+    array.join(" and ")
+  when array.length == 1 
+    array.join
+end 
+end
+katzDeli = []
+
+def line(katzDeli)
+  case
+  when katzDeli.length >
+end
+
