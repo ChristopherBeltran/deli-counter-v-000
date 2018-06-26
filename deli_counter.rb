07acf080a9ad64f476d4f5d12518ef78b2102d6e
Christopher Beltran <christod.beltran@gmail.com>
2018-06-26 06:33:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |person, number|
       this_line << "#{number + 1}. #{person}"
     end 
-      the_line = this_line.join
+      the_line = this_line.join(" ")
       puts "The line is currently: #{the_line}"
   end 
 end 
